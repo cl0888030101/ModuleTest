00237f1c59fac6e2bb5effd4e62b57f8b03f82db
Long Cheng <cl0888030101@gmail.com>
2020-06-10 10:44:28 -0700

    recreate develop branch test

--- FirstModule/src/main/java/com/longcheng/firstapp/MainActivity.java
+++ FirstModule/src/main/java/com/longcheng/firstapp/MainActivity.java
@@ -12,9 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("hello world");
-        System.out.println("hello world two");
-        System.out.println("hello world three");
+        System.out.println("develop branch recreate");
         Intent i = new Intent(MainActivity.this, com.longcheng.secondapp.MainActivity.class);
         startActivity(i);
     }
